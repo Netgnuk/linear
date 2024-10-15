99c808651e9c5fcb121fcc562835cc2ce128db55
Netgnuk <142572868+Netgnuk@users.noreply.github.com>
2024-10-15 20:02:43 +0700

    Add files via upload

--- HumenPerfume-main/HumenPerfume-main/base_note.py
+++ HumenPerfume-main/HumenPerfume-main/base_note.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv('base.csv')
 
 # กรองเอาเฉพาะค่าตัวแรก
-first_items = df['base_notes'].unique()  # แทนที่ 'Base Note' ด้วยชื่อคอลัมน์ที่คุณใช้
+first_items = df['base'].unique()  # แทนที่ 'Base Note' ด้วยชื่อคอลัมน์ที่คุณใช้
 
 # สร้าง JSON
-json_data = [{'base_notes': item} for item in first_items]
+json_data = [{'base': item} for item in first_items]
 
 # บันทึกเป็นไฟล์ JSON
 with open('base_notes.json', 'w', encoding='utf-8') as f:
